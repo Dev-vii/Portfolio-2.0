1acaff8e845fd7554b004da8277c452862127cf0
Vicente <carvalho.vicente0207@gmail.com>
2025-04-28 23:00:24 -0300

    Seção de tecnologias melhorada

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ header.addEventListener('mouseleave', () => {
     header.style.top = "-35px"
     header.style.transition = "0.5s"
 });
+
+const ptech = document.getElementById("techp")
+
+
+const html = document.getElementById("html").addEventListener('mouseenter', () => {
+    ptech.style.display = "block"
+    ptech.innerHTML = "HTML5"
+})
+
+const css = document.getElementById("css").addEventListener('mouseenter', () => {
+    ptech.style.display = "block"
+    ptech.innerHTML = "CSS3"
+})
+
+const js = document.getElementById("js").addEventListener('mouseenter', () => {
+    ptech.style.display = "block"
+    ptech.innerHTML = "JavaScript"
+})
+
+const git = document.getElementById("git").addEventListener('mouseenter', () => {
+    ptech.style.display = "block"
+    ptech.innerHTML = "Git e GitHub"
+})
+
+const react = document.getElementById("react").addEventListener('mouseenter', () => {
+    ptech.style.display = "block"
+    ptech.innerHTML = "React.js"
+})
