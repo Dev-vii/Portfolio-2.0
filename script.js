e97bb9eeb85519feec538e3579906d374f5ebb2f
Vicente <carvalho.vicente0207@gmail.com>
2026-04-28 18:29:08 -0300

    tentativa de inserir modelos 3d

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('wheel', (e) => {
     // Impedir o scroll vertical padrão
     e.preventDefault();
-    
+
     // Converter movimento vertical em movimento horizontal com multiplicador
     window.scrollBy({
         left: e.deltaY * 8,
